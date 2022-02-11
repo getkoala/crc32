1f8ef05acddb975b5cafe39aca293ae934e49fb6
Netto Farah <nettofarah@gmail.com>
2022-02-11 10:10:29 -0300

    Remove node_modules

---
+++ README.md
@@ -0,0 +1,14 @@
+# CRC32
+A tiny implementation of the CRC32 algorithm in JavaScript
+
+## Usage
+
+```bash
+$ npm install --save @koala-live/crc32
+```
+
+```javascript
+import crc32 from '@koala-live/crc32'
+
+crc32('koala 🐨') // 4005375993
+```
--- tests/index.test.js
+++ tests/index.test.js
@@ -6,6 +6,7 @@ import { crc32 } from '../src';
 test('hashes the input correctly', function () {
   assert.equal(crc32('foobar'), 2666930069);
   assert.equal(crc32('Magna Pellentesque Egestas Nibh Ultricies'), 1920919084);
+  assert.equal(crc32('koala 🐨'), 4005375993);
 });
 
 test.run();
